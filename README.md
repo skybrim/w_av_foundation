aa026b9a577a3eca2671b27255fc5ccfe6dc385d
wiley <throughskybrim@gmail.com>
2021-01-23 01:15:40 +0800

    add fdk-aac

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # w_av_foundation
+
+## 编译
+
+开源库的源代码版本默认最新的release（尽量保持更新），以压缩包形式放在仓库中
+默认编译架构是 arm64 x86_64 i386 armv7
+
+### 编译fdk-aac
+
+```
+cd fdk-aac
+tar xvf fdk-aac-2.0.1.tar.gz
+sh build_fdk-aac.sh
+```
+
+编译最终结果在 fdk-aac-fat-iOS 文件中
---
+++ fdk-aac/build_fdk-aac.sh
@@ -0,0 +1,92 @@
+#! /bin/sh
+
+SOURCE="fdk-aac-2.0.1"
+FAT="fdk-aac-fat-iOS"
+SCRATCH="scratch"
+THIN=`pwd`/"thin"
+
+CONFIGURE_FLAGS="--enable-static --disable-shared --with-pic=yes"
+
+ARCHS="arm64 x86_64 i386 armv7"
+DEPLOYMENT_TARGET="8.0"
+
+CWD=`pwd`
+cd $SOURCE
+autoreconf -i
+cd $CWD
+
+for ARCH in $ARCHS
+do
+  echo "building $ARCH..."
+  mkdir -p "$SCRATCH/$ARCH"
+  cd "$SCRATCH/$ARCH"
+
+  CFLAGS="-arch $ARCH"
+
+  if [ "$ARCH" = "i386" -o "$ARCH" = "x86_64" ]
+  then
+    PLATFORM="iPhoneSimulator"
+    CPU=
+    if [ "$ARCH" = "i386" ]
+    then
+      CFLAGS="$CFLAGS -mios-simulator-version-min=7.0"
+      HOST="--host=i386-apple-darwin"
+    else
+      CFLAGS="$CFLAGS -mios-simulator-version-min=7.0"
+      HOST="--host=x86_64-apple-darwin"
+    fi
+  else
+    PLATFORM="iPhoneOS"
+    if [ "$ARCH" = "arm64" ]
+    then
+      HOST="--host=aarch64-apple-darwin"
+    else
+      HOST="--host=arm-apple-darwin"
+    fi
+    CFLAGS="$CFLAGS -fembed-bitcode"
+  fi
+
+  XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+  CC="xcrun -sdk $XCRUN_SDK clang"
+  AS="$CWD/$SOURCE/extras/gas-preprocessor.pl $CC"
+  CXXFLAGS="$CFLAGS"
+  LDFLAGS="$CFLAGS"
+
+  $CWD/$SOURCE/configure \
+      $CONFIGURE_FLAGS \
+      $HOST \
+      $CPU \
+      CC="$CC" \
+      CXX="$CC" \
+      CPP="$CC -E" \
+      AS="$AS" \
+      CFLAGS="$CFLAGS" \
+      LDFLAGS="$LDFLAGS" \
+      CPPFLAGS="$CFLAGS" \
+      --prefix="$THIN/$ARCH"
+
+  make -j6 install
+  cd $CWD
+done
+
+# 合并FAT
+echo "building fat binaries..."
+mkdir -p $FAT/lib
+set - $ARCHS
+CWD=`pwd`
+cd $THIN/$1/lib
+for LIB in *.a
+do
+  cd $CWD
+  lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB
+done
+
+cd $CWD
+cp -rf $THIN/$1/include $FAT
+
+# 验证FAT
+cd $FAT/lib
+for LIB in *.a
+do
+  lipo -info $LIB
+done
