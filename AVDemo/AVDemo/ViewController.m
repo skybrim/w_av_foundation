bb9529f7f7e1079e718a6f138d28aec7c9fea3e8
wiley <throughskybrim@gmail.com>
2021-01-25 18:04:09 +0800

    AVDemo 引入 FFmpeg静态库

--- AVDemo/AVDemo/ViewController.m
+++ AVDemo/AVDemo/ViewController.m
@@ -7,6 +7,12 @@
 
 #import "ViewController.h"
 
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libswresample/swresample.h"
+#include "libavutil/pixdesc.h"
+
+
 @interface ViewController ()
 
 @end
@@ -16,6 +22,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+    av_log(NULL, AV_LOG_ERROR, "error");
 }
 
 
